ec002028112a1cfb20734ebcc3a3f47fb65071de
Hunter Powers <hunter@wiretheplanet.com>
2012-06-20 14:05:41 -0400

    setup routing and model relationships

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,9 @@
 class Movie < ActiveRecord::Base
+  has_many :rotten_tomatoes , dependent: :destroy
+  has_many :imdb_data_points, dependent: :destroy
+  has_many :tweets          , dependent: :destroy
+  has_many :ebert_stars     , dependent: :destroy
+  has_many :hsx_data_points , dependent: :destroy
+  
   attr_accessible :default, :score, :title
 end
