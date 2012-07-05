4b8abb4b55943ff5499a4c2be5a56d04c61cab38
Hunter Powers <hunter@wiretheplanet.com>
2012-07-05 08:50:53 -0400

    spec for Movies_pages integrations specifications

---
+++ spec/requests/movie_pages_spec.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe "Movie pages" do
+  
+  it "should link back to first movie after after last" do
+    # Movie.count.times { click_link 'Next →' }
+  end
+
+end
