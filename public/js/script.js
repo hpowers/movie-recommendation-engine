582d73ace4d0c031c268aee9f33113dad2ce1af5
Hunter Powers <hunter@wiretheplanet.com>
2012-07-06 16:37:20 -0400

    mobile movies view implemented

--- public/js/script.js
+++ public/js/script.js
@@ -4,11 +4,37 @@ $(document).ready(function() {
   // Stuff to do as soon as the DOM is ready;
   $('#info').click(function(event) {
     $('#info_box').slideToggle();
+    $('#recommendation').toggleClass('pointer');
     return false;
   });
   $('#showtimes').click(function(event) {
     // Act on the event
-    var count = prompt("Enter Your Zip Code");
+    var count = prompt("Enter Your Zips Code");
     return false;
   });
+
+  $('#recommendation').click(function(event) {
+    // Act on the event
+    $('#info_box').slideToggle();
+    $('#recommendation').toggleClass('pointer');
+    return false;
+  });
+
+  fitTitle();
+
+
 });
+
+
+function fitTitle(){
+  var max_width = 310;
+  var max_height = 175;
+  var title = $('#recommendation h1');
+
+  while (title.width() > max_width || title.height() > max_height) {
+    var current_size = parseInt(title.css('font-size'));
+
+    $('#recommendation h1').css('font-size',(current_size-1)+'px');
+  }
+
+}
