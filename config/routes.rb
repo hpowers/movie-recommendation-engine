ec35afa3691690e84dcd068216803f3c48911456
Hunter Powers <hunter@wiretheplanet.com>
2012-07-14 06:24:10 -0400

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,8 @@ MovieRecommendationEngine::Application.routes.draw do
   resources :data, only: [:index]
 
   resources :movies, only: [:index, :show, :update] do
-    member do
-      get 'score'
-    end
     resources :theaters, only: [:show, :create]
   end
-
-  # match "/movies/score/:id" => "movie#score"
   
   root to: 'movies#index'
 
