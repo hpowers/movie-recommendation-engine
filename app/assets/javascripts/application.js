5e571511c76f6099e4d07b83819bd48fc398e6d0
Hunter Powers <hunter@wiretheplanet.com>
2012-07-18 10:00:56 -0400

    refactor js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 //= require handlebars-1.0.0.beta.6.js
 //= require dataTables/jquery.dataTables
 //= require jRespond.min.js
+//= require_tree .
