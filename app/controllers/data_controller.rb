5e2c3dde88abb2678dc163a2831d9a5b9b3d9dca
Hunter Powers <hunter@wiretheplanet.com>
2012-07-22 15:28:40 -0400

    cleanup

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -2,7 +2,6 @@ class DataController < ApplicationController
   before_filter :authenticate
 
   def index
-    # @movies = Movie.released
     @movies = Movie.joins(:hsx_datum).where('theaters > 1')
   end
 
